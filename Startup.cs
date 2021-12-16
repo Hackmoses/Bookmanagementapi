0ba9124a90f43c3f73ce025c3a7ee067f4e71e7c
Hackmoses <atabenataben1@gmail.com>
2021-12-16 14:30:56 +0100

    ft/added swagger

--- Startup.cs
+++ Startup.cs
@@ -111,12 +111,12 @@ namespace Bookmanagementapi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider svp)
         {
-            if (env.IsDevelopment())
-            {
+           /* if (env.IsDevelopment())
+            { */
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Bookmanagementapi v1"));
-            }
+           /* } */
 
             app.UseHttpsRedirection();
 
